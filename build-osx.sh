4ed164e027c4da5f6708e1ce755cd0b0f650d7e7
hideki <hideki@couchbase.com>
2016-01-04 15:47:16 -0800

    update for OS X

--- build-osx.sh
+++ build-osx.sh
@@ -15,17 +15,14 @@ build_osx() {
 		./runConfigureICU MacOSX --enable-static
 	fi
 	make clean
-	cd ./common
 	make
-	cd ..
-	cd ./i18n
-	make
-	cd ..
 	file lib/libicuuc.a
 	file lib/libicui18n.a
+	file lib/libicudata.a.a
 	mkdir -p ../${OUTPUT_DIR}/${ARCH}/
 	cp lib/libicuuc.a ../${OUTPUT_DIR}/${ARCH}/
 	cp lib/libicui18n.a ../${OUTPUT_DIR}/${ARCH}/
+	cp lib/libicudata.a ../${OUTPUT_DIR}/${ARCH}/
 }
 
 build_osx "x86"
