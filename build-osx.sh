a4a94a9b348f18058231f880e75fb3b935642c08
hideki <hideki@couchbase.com>
2016-01-05 13:36:06 -0800

    update OS X build scripts and libraries

--- build-osx.sh
+++ build-osx.sh
@@ -10,15 +10,16 @@ build_osx() {
 	ARCH=$1
   	echo "Building osx libicuuc.a and libicui18n.a for ${ARCH}"
   	if [[ $ARCH == "x86" ]]; then
+  		# With --disable-shared option, build for libicudata.a fails for 32bit. 
   		./runConfigureICU MacOSX --enable-static --with-library-bits=32
   	else
-		./runConfigureICU MacOSX --enable-static
+		./runConfigureICU MacOSX --enable-static 
 	fi
 	make clean
 	make
 	file lib/libicuuc.a
 	file lib/libicui18n.a
-	file lib/libicudata.a.a
+	file lib/libicudata.a
 	mkdir -p ../${OUTPUT_DIR}/${ARCH}/
 	cp lib/libicuuc.a ../${OUTPUT_DIR}/${ARCH}/
 	cp lib/libicui18n.a ../${OUTPUT_DIR}/${ARCH}/
